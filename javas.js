688ccfdcab8f4473ef49f52ce026051988d47298
Ezio2220 <fjosue447@gmail.com>
2019-03-25 19:59:56 -0600

    Add files via upload

---
+++ javas.js
@@ -0,0 +1,41 @@
+var n=1;
+var Pn=0;
+var f=0;
+var an=0;
+var bn=0;
+var Er=0;
+var fp=0;
+var tol=0;
+var criterio;
+var ax=0;
+var tab=document.getElementById("contenido");
+function calcular(){
+    an=document.getElementById("an").value;
+    bn=document.getElementById("bn").value;
+    f=document.getElementById("fn").value;
+    tol=parseFloat(Math.pow(10,-document.getElementById("tol").value));
+    criterio = document.getElementById("crit").value;
+    do{
+    ax=(parseFloat(an)+parseFloat(bn))/2;
+    f=f.replace("x",ax);
+    fp=eval(f);
+    if(n==1){
+        Er=0;
+    }else{
+        Er=(parseFloat(ax)-parseFloat(Pn))/parseFloat(ax);
+    }
+    Pn=ax;
+    tab=document.getElementById("contenido");
+    tab.innerHTML= "<tr> <th scope='row'>"+n+"</th>"+
+    "<td>"+an+"</td><td>"+bn+"</td>"+
+    "<td>"+Pn+"</td><td>"+fp+"</td>"+
+    "<td>"+Er+"</td></tr>";
+    if(fp>0){
+        bn=Pn;
+    }else{
+        an=Pn
+    }
+    n++;
+    }while(n==3);
+
+}
